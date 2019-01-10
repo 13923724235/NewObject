dcf04c6e2c70417a1310dc8e4ff66f09b3e9c69e
13923724235 <735917654@qq.com>
2019-01-10 11:09:07 +0800

    Initial Commit

---
+++ NewObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewObject
+//
+//  Created by addcn on 2019/1/10.
+//  Copyright © 2019年 addcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewObject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  NewObject
+//
+//  Created by addcn on 2019/1/10.
+//  Copyright © 2019年 addcn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
